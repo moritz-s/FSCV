9ffd73c84018ab55057343700ed2f51faaa30687
Moritz Schlötter <moritz.schloetter@uni-konstanz.de>
2021-11-15 22:37:02 +0100

    gui wrk: stop button and bugfixes

--- fscv.py
+++ fscv.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import time
 import numpy as np
 import tables as tb
 
@@ -12,9 +13,9 @@ import pyqtgraph.widgets.RemoteGraphicsView
 
 
 class FscvWin(QtGui.QMainWindow):
+    """Main window for the FSCV measurement"""
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
-############################## ##############################
         area = self.area = pqda.DockArea()
         self.setCentralWidget(area)
         self.resize(1200, 500)
@@ -43,11 +44,15 @@ class FscvWin(QtGui.QMainWindow):
             ]},
             {'name': 'Run', 'type': 'group', 'children': [
                 {'name': 'Start', 'type': 'action'},
-                {'name': 'Stop', 'type': 'action'},
+                {'name': 'Stop', 'type': 'action', 'enabled': False},
                 {'name': 'Limit (N)', 'type': 'int', 'value': 0},
             ]},
             {'name': 'Monitor', 'type': 'group', 'children': [
                 {'name': 'Aquisition frequency', 'type': 'float', 'value': 0, 'siPrefix': True, 'suffix': 'Hz',
+                 'readonly': True},
+            ]},
+            {'name': 'DAQ', 'type': 'group', 'children': [
+                {'name': 'Database', 'type': 'str', 'value': '',
                  'readonly': True},
             ]},
             ]
@@ -70,7 +75,7 @@ class FscvWin(QtGui.QMainWindow):
 
         #p.sigTreeStateChanged.connect(change)
         p.param('Run', 'Start').sigActivated.connect(self.start_recording)
-        #p.param('Run', 'Stop').sigActivated.connect(restore)
+        p.param('Run', 'Stop').sigActivated.connect(self.stop_recording)
         t = ParameterTree()
         t.setParameters(p, showTop=False)
         t.setWindowTitle('pyqtgraph example: Parameter Tree')
@@ -108,7 +113,7 @@ class FscvWin(QtGui.QMainWindow):
         im_data = np.ones((100, 200)) * np.linspace(0, 100, 200)
         self.im_rplt.setImage(im_data.T)#np.ascontiguousarray(im_data))
 
-        self.lastUpdate = pg.ptime.time()
+        self.lastUpdate = time.perf_counter()
         self.avgFps = 0.0
         self.start_recording()
 
@@ -121,10 +126,13 @@ class FscvWin(QtGui.QMainWindow):
         self.array_scans.append(data[:, np.newaxis])
 
         #self.im_rplt.setImage(self.array_imgs)#np.ascontiguousarray(im_data))
-        self.im_rplt.setImage(np.array(self.array_scans))#np.ascontiguousarray(im_data))
+        self.im_rplt.setImage(np.array(self.array_scans), autoLevels = False,
+                     autoHistogramRange = False)#, autoRange = False)
+        #np.ascontiguousarray(im_data))
+        #self.imv.setImage(imgarr, autoLevels=False,
+         #                 autoHistogramRange=False, autoRange=False)
 
-
-        now = pg.ptime.time()
+        now = time.perf_counter()
         fps = 1.0 / (now - self.lastUpdate)
         self.lastUpdate = now
         self.avgFps = self.avgFps * 0.8 + fps * 0.2
@@ -132,6 +140,9 @@ class FscvWin(QtGui.QMainWindow):
         #self.label.setText("Generating %0.2f fps" % self.avgFps)
 
     def start_recording(self):
+        self.p.param('Run').param('Start').setOpts(enabled=False)
+        self.p.param('Run').param('Stop').setOpts(enabled=True)
+
         fln = "test.h5" #self.mtree.param('Filename').value() + '.h5'
         dataroot = "." #self.mtree.param('Dataroot').value()
         self.fileh = tb.open_file(os.path.join(dataroot, fln), mode='w')
@@ -139,8 +150,8 @@ class FscvWin(QtGui.QMainWindow):
         H = 100
         complevel = 5#np.int(self.mtree.param("BloscLevel").value())
         filters = tb.Filters(complevel=complevel, complib='blosc')
-        self.array_scans = self.fileh.create_earray(self.fileh.root, 'array_imgs', tb.FloatAtom(),
-                                              (H, 0), "Imgs",
+        self.array_scans = self.fileh.create_earray(self.fileh.root, 'array_scans', tb.FloatAtom(),
+                                              (H, 0), "Scans",
                                               filters=filters,
                                               expectedrows=500)
 
@@ -148,6 +159,30 @@ class FscvWin(QtGui.QMainWindow):
         self.timer.timeout.connect(self.update)
         self.timer.start(10)
 
+    def stop_recording(self):
+        self.p.param('Run').param('Start').setOpts(enabled=True)
+        self.p.param('Run').param('Stop').setOpts(enabled=False)
+        self.timer.stop()
+        self.fileh.close()
+
+    def closeEvent(self, event):
+        if 1:
+            reply = QtGui.QMessageBox.question(self, 'Message',
+                                               "Are you sure to quit?",
+                                               QtGui.QMessageBox.Yes,
+                                               QtGui.QMessageBox.No)
+            if reply == QtGui.QMessageBox.No:
+                event.ignore()
+                return
+
+        if self.p.param('Run').param('Stop').opts['enabled']:
+            self.stop_recording()
+        #self.timer.stop()
+        self.view.close()
+        #self.camera.StopGrabbing()
+        # camera has to be closed manually
+        #self.camera.Close()
+        event.accept()
 
 #app = pg.mkQApp()
 #window = FscvWin()
@@ -175,6 +210,9 @@ def main(**kwargs):
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
+    # Hide error from QMessageBox
+    os.environ["QT_LOGGING_RULES"] = "*.debug=false"
+
     #window, app = main()
     app = QtGui.QApplication(sys.argv)
     window = FscvWin()
